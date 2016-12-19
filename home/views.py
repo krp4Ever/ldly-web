ae90e1e71549d7f8edab887bd65d4d4283047e69
pankairan <pankairan@baidu.com>
2016-12-19 20:05:32 +0800

    slider animate

--- home/views.py
+++ home/views.py
@@ -2,8 +2,19 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 # Create your views here.
+from django.contrib.staticfiles.templatetags.staticfiles import static
 def index(request):
 	return render_to_response('home/index.html',{
+        'sliderContent': [
+            {
+                'img':static('images/content/slider_pic1.jpg'),
+                'desc':'推进国际产能和装备制造合作，是保持我国经济中高速增长和迈向中高端 水平的重大举措'
+            },
+            {
+                'img':static('images/content/slider_pic2.jpg'),
+                'desc':'推进国际产能和装备制造合作，是保持我国经济中高速增长和迈向中高端 水平的重大举措'
+            }
+        ],
         'channelList':{
             'title':'国别投资报告',
             'dataArray': [
